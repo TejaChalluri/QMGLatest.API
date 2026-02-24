3d96d9cc62c3a6fac822394ed6469c9e6b198e82
Teja Challuri <teja.c@paccore.com>
2026-02-24 21:00:42 +0530

    2 fields added user model

--- QMGLatest.API/Models/User.cs
+++ QMGLatest.API/Models/User.cs
@@ -17,6 +17,8 @@
 public class RegisterRequest
 {
     public int UserId { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
